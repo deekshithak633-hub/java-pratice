35c8822e14295c06ce882f68a62c6f98d7a1fe11
Deekshitha <deekshithak633@gmail.com>
2025-12-18 14:17:08 +0530

    Add MethodExample class with greeting method

---
+++ basics/basics/basics/basics/MethodExample.java
@@ -0,0 +1,10 @@
+class MethodExample {
+
+    static void greet() {
+        System.out.println("Welcome to Java");
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
